1ca81fb7bf60ac2a73dbf27e9113db8dbc1f6264
Harley <143757576+chaezuha@users.noreply.github.com>
2026-05-03 16:40:05 -0700

    Added neovim config

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      c = { "clang-format" },
+      cpp = { "clang-format" },
+      python = { "black" },
+      lua = { "stylua" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      markdown = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      rust = { "rustfmt" },
+      java = { "google-java-format" },
+      sh = { "shfmt" },
+    },
+  },
+}
